b0d57fd443d48216e83c20f4f1a09a9f3ec830a9
miathedev <mia@metzler.systems>
2022-11-29 16:51:10 +0100

    car_detection_red

--- test/ParkingGarageTest.py
+++ test/ParkingGarageTest.py
@@ -9,7 +9,24 @@ from mock.RTC import RTC
 
 
 class ParkingGarageTest(unittest.TestCase):
-    """
-    Your test methods go here
-    """
-    pass
+    def setUp(self):
+        self.parking_garage = ParkingGarage()
+    @patch("mock.GPIO.input")
+    def test_parking_garage_check_occupancy_true(self, mock_input):
+        #mock_input.side_effect = [10,20,30,40,50,60]
+        mock_input.return_value = 1023 #Any non zero value means, a car is detected
+        for num in range(11, 14): #11,12,13
+            print("Check: ", num)
+            occupancy = self.parking_garage.check_occupancy(num)
+            self.assertTrue(occupancy)
+
+    @patch("mock.GPIO.input")
+    def test_parking_garage_check_occupancy_false(self, mock_input):
+        # mock_input.side_effect = [10,20,30,40,50,60]
+        mock_input.return_value = 0 #Zero value: nothing is detected in front of the sensor
+        for num in range(11, 14):  # 11,12,13
+            print("Check: ", num)
+            occupancy = self.parking_garage.check_occupancy(num)
+            self.assertFalse(occupancy)
+
+
